9beddf82a73886c007d9500a29e797b510d2ae3a
V.Vitaliy <vitaliyverzyn25@gmail.com>
2024-04-01 11:26:41 +0300

    preparation & new UserEntity class

---
+++ src/main/java/com/example/ecommercemarketplace/models/UserEntity.java
@@ -0,0 +1,43 @@
+package com.example.ecommercemarketplace.models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "public_id", nullable = false, unique = true)
+    private String publicId;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private boolean isEnabled;
+
+}
