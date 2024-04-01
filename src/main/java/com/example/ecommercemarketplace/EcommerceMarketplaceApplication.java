fe7e22d2b7011a40b118413ef18e2e6f23c26e07
V.Vitaliy <vitaliyverzyn25@gmail.com>
2024-04-01 10:55:12 +0300

    initialization

---
+++ src/main/java/com/example/ecommercemarketplace/EcommerceMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommercemarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceMarketplaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ecommercemarketplace/EcommerceMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommercemarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
