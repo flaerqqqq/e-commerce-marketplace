b80052936864dc6d5f0989087d24d70c8cc5bce4
V.Vitaliy <vitaliyverzyn25@gmail.com>
2024-04-30 12:17:31 +0300

    refactored exceptions folder

--- src/main/java/com/example/ecommercemarketplace/exceptions/PasswordResetTokenNotFoundException.java
+++ src/main/java/com/example/ecommercemarketplace/exceptions/PasswordResetTokenNotFoundException.java
@@ -1,2 +1,7 @@
-package com.example.ecommercemarketplace.exceptions;public class PasswordResetTokenNotFoundException {
+package com.example.ecommercemarketplace.exceptions;
+
+public class PasswordResetTokenNotFoundException extends RuntimeException{
+    public PasswordResetTokenNotFoundException(String msg){
+        super(msg);
+    }
 }
