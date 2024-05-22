64f942c33923b3570c9d905fa0a08181c55c946f
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-05-22 13:46:36 +0300

    implemented http request logging (main)

--- src/main/java/com/example/ecommercemarketplace/logging/HttpLogMessage.java
+++ src/main/java/com/example/ecommercemarketplace/logging/HttpLogMessage.java
@@ -1,2 +1,35 @@
-package com.example.ecommercemarketplace.logging;public class HttpLogMessage {
+package com.example.ecommercemarketplace.logging;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+@Builder
+public class HttpLogMessage {
+
+    private String requestId;
+    private Request request;
+    private Response response;
+
+    @Data
+    @Builder
+    public static class Request {
+        private String method;
+        private String url;
+        private String clientIp;
+        private String body;
+        private Map<String, String> headers;
+    }
+
+    @Data
+    @Builder
+    public static class Response {
+        private int status;
+        private String body;
+    }
 }
+
+
+
