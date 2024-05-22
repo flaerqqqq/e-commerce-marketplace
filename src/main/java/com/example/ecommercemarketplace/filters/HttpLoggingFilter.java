4f088623a4e4e90e9c79b43f15188aad3abf6d0c
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-05-22 13:45:45 +0300

    implemented http request logging

---
+++ src/main/java/com/example/ecommercemarketplace/filters/HttpLoggingFilter.java
@@ -0,0 +1,2 @@
+package com.example.ecommercemarketplace.filters;public class HttpLoggingFilter {
+}
---
+++ src/main/java/com/example/ecommercemarketplace/logging/HttpLogMessage.java
@@ -0,0 +1,2 @@
+package com.example.ecommercemarketplace.logging;public class HttpLogMessage {
+}
