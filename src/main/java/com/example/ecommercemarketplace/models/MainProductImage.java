26570f4441c164587d5e8216d7fa88929f5f3355
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-05-30 14:15:02 +0300

    created entities for product images

---
+++ src/main/java/com/example/ecommercemarketplace/models/MainProductImage.java
@@ -0,0 +1,27 @@
+package com.example.ecommercemarketplace.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "main_product_images")
+public class MainProductImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String url;
+
+    @OneToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
---
+++ src/main/java/com/example/ecommercemarketplace/models/ProductImage.java
@@ -0,0 +1,28 @@
+package com.example.ecommercemarketplace.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "product_images")
+public class ProductImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String url;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
+
