748d79a1ee1d7e852fca8e3b2f77ffa8eb43d8b8
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-05-31 20:47:45 +0300

    created image validation based exceptions

---
+++ src/main/java/com/example/ecommercemarketplace/exceptions/InvalidImageAspectRatioException.java
@@ -0,0 +1,7 @@
+package com.example.ecommercemarketplace.exceptions;
+
+public class InvalidImageAspectRatioException extends RuntimeException {
+    public InvalidImageAspectRatioException(String msg) {
+        super(msg);
+    }
+}
