6e2983aebc541cef78e8ab2dad1f7acaff889513
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-06-02 14:41:56 +0300

    created endpoint method for creating product review

--- src/main/java/com/example/ecommercemarketplace/controllers/ProductController.java
+++ src/main/java/com/example/ecommercemarketplace/controllers/ProductController.java
@@ -3,14 +3,18 @@ package com.example.ecommercemarketplace.controllers;
 
 import com.example.ecommercemarketplace.dto.ProductDto;
 import com.example.ecommercemarketplace.dto.ProductResponseDto;
+import com.example.ecommercemarketplace.dto.ProductReviewRequestDto;
+import com.example.ecommercemarketplace.dto.ProductReviewResponseDto;
+import com.example.ecommercemarketplace.services.ProductReviewService;
 import com.example.ecommercemarketplace.services.ProductService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -21,6 +25,7 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService productService;
+    private final ProductReviewService productReviewService;
 
     @GetMapping
     public Page<ProductResponseDto> getAllProducts(Pageable pageable){
@@ -31,5 +36,14 @@ public class ProductController {
     public Page<ProductResponseDto> searchProductsByName(@RequestParam String query,
                                                          Pageable pageable){
         return productService.searchProducts(query, pageable);
+    }
+
+    @PostMapping("/{id}/product-reviews")
+    @PreAuthorize("hasRole('USER')")
+    public ProductReviewResponseDto createProductReview(@PathVariable("id") Long productId,
+                                                        @RequestPart("text") @Valid ProductReviewRequestDto createRequest,
+                                                        @RequestPart("media") List<MultipartFile> mediaContent,
+                                                        Authentication authentication){
+        return productReviewService.createProductReview(productId, createRequest, mediaContent, authentication);
     }
 }
