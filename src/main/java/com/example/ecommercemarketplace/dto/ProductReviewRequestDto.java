9e4a5e8fc70ed1a143cdcfc4d65d1c562d90dbb8
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-06-02 14:39:21 +0300

    created dto for product review creation request

---
+++ src/main/java/com/example/ecommercemarketplace/dto/ProductReviewRequestDto.java
@@ -0,0 +1,16 @@
+package com.example.ecommercemarketplace.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductReviewRequestDto {
+
+    private String textContent;
+}
