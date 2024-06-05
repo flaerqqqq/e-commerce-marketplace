c76db3872ba180230a5d6d303876b50ccdf9a6af
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-06-05 17:26:10 +0300

    implemented media content for product reviews validation

--- src/main/java/com/example/ecommercemarketplace/mappers/UserMapper.java
+++ src/main/java/com/example/ecommercemarketplace/mappers/UserMapper.java
@@ -4,12 +4,10 @@ import com.example.ecommercemarketplace.dto.UserDto;
 import com.example.ecommercemarketplace.models.UserEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.NullValueMappingStrategy;
 
 @Mapper(uses = {ProductReviewMapper.class})
 public interface UserMapper {
 
-    @Mapping(target = "productReviews", ignore = true)
     @Mapping(source = "enabled", target = "isEnabled")
     UserEntity mapFrom(UserDto userDto);
 
--- src/main/java/com/example/ecommercemarketplace/services/impls/ProductReviewServiceImpl.java
+++ src/main/java/com/example/ecommercemarketplace/services/impls/ProductReviewServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.ecommercemarketplace.services.impls;
 
 import com.example.ecommercemarketplace.dto.ProductReviewRequestDto;
 import com.example.ecommercemarketplace.dto.ProductReviewResponseDto;
+import com.example.ecommercemarketplace.exceptions.UnsupportedContentFileTypeException;
 import com.example.ecommercemarketplace.mappers.ProductReviewMapper;
 import com.example.ecommercemarketplace.models.ProductReview;
 import com.example.ecommercemarketplace.models.ProductReviewMediaContent;
@@ -11,6 +12,8 @@ import com.example.ecommercemarketplace.repositories.UserRepository;
 import com.example.ecommercemarketplace.services.ProductReviewService;
 import com.example.ecommercemarketplace.services.ProductService;
 import com.example.ecommercemarketplace.services.ReviewMediaFileBucketService;
+import com.example.ecommercemarketplace.validation.ImageValidator;
+import com.example.ecommercemarketplace.validation.VideoValidator;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -42,6 +45,7 @@ public class ProductReviewServiceImpl implements ProductReviewService {
         productReview.setUser(userRepository.findByEmail(authentication.getName()).get());
 
         if (mediaContent != null) {
+            validateMediaContents(mediaContent);
             List<ProductReviewMediaContent> uploadedMediaContent = reviewMediaFileBucketService.uploadProductMediaContent(mediaContent);
             uploadedMediaContent.forEach(media -> media.setProductReview(productReview));
             productReview.setMediaContents(uploadedMediaContent);
@@ -49,5 +53,21 @@ public class ProductReviewServiceImpl implements ProductReviewService {
 
         ProductReview savedProductReview = productReviewRepository.save(productReview);
         return productReviewMapper.mapToResponseDto(savedProductReview);
+    }
+
+    private void validateMediaContents(List<MultipartFile> mediaContents){
+        for (MultipartFile mediaContent : mediaContents){
+            validateMediaContentFile(mediaContent);
+        }
+    }
+
+    private void validateMediaContentFile(MultipartFile mediaContentFile){
+        if (mediaContentFile.getContentType().startsWith("image/")) {
+            ImageValidator.validateFile(mediaContentFile);
+        } else if (mediaContentFile.getContentType().startsWith("video/")){
+            VideoValidator.validateFile(mediaContentFile);
+        } else {
+            throw new UnsupportedContentFileTypeException("File with type=%s is not supported".formatted(mediaContentFile.getContentType()));
+        }
     }
 }
