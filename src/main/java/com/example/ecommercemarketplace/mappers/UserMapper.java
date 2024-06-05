4e62feb5afcb753cd55708a1a793021d6342a514
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-06-05 17:24:26 +0300

    fixed mapFrom method in UserMapper

--- src/main/java/com/example/ecommercemarketplace/mappers/UserMapper.java
+++ src/main/java/com/example/ecommercemarketplace/mappers/UserMapper.java
@@ -4,10 +4,12 @@ import com.example.ecommercemarketplace.dto.UserDto;
 import com.example.ecommercemarketplace.models.UserEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.NullValueMappingStrategy;
 
 @Mapper(uses = {ProductReviewMapper.class})
 public interface UserMapper {
 
+    @Mapping(target = "productReviews", ignore = true)
     @Mapping(source = "enabled", target = "isEnabled")
     UserEntity mapFrom(UserDto userDto);
 
