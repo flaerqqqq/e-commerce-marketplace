cf71d5fd7bf6b41e010aed8d2f130c83067d50f2
flaerqqqq <vitaliyverzyn25@gmail.com>
2024-06-08 14:39:08 +0300

    refactored application runner codestyle

--- src/main/java/com/example/ecommercemarketplace/EcommerceMarketplaceApplication.java
+++ src/main/java/com/example/ecommercemarketplace/EcommerceMarketplaceApplication.java
@@ -1,24 +1,14 @@
 package com.example.ecommercemarketplace;
 
-import com.example.ecommercemarketplace.services.LoginAttemptEmailService;
-import com.example.ecommercemarketplace.services.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @AllArgsConstructor
-public class EcommerceMarketplaceApplication implements CommandLineRunner{
-
-    private LoginAttemptEmailService loginAttemptEmailService;
+public class EcommerceMarketplaceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EcommerceMarketplaceApplication.class, args);
-    }
-
-
-    @Override
-    public void run(String... args) throws Exception {
     }
 }
